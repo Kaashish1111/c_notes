09e44e11b3472b298c1ce4df81fdfa0f0371eec8
Kashish Goyal <kashish1782004@gmail.com>
2024-05-09 12:21:19 +0530

    question

--- structures/question1.c
+++ structures/question1.c
@@ -1,1 +1,13 @@
 #include<stdio.h>
+struct question
+{
+    /* data */
+    int a;
+    float b;
+    char c;
+};
+int main(){
+    struct question q={.b=1,.c=10,.a=15};
+    printf("%d %f %d",q.a,q.b,q.c);
+    return 0;
+}
