ec7c3b56d8cb37e8bfb597e1f9e5b8b6267a0922
Kashish Goyal <kashish1782004@gmail.com>
2024-05-19 22:15:24 +0530

    malloc

---
+++ dynamic_memory_Allocation/malloc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n,*ptr;
+    printf("Enter total no of values that you want to enter: ");
+    scanf("%d",&n);
+    ptr=(int *)malloc(n*sizeof(int));
+    printf("Enter values: \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",(ptr+i));
+    }
+    printf("The values you entered is: \n");
+    for(int i=0;i<n;i++){
+        printf("%d\t",*(ptr+i));
+    }
+}
