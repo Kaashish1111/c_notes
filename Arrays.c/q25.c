c26d5e6820f5b6dc7684aac04bb28ccd75e3f597
Kashish Goyal <kashish1782004@gmail.com>
2024-09-02 08:47:55 +0530

    arrays done

---
+++ Arrays.c/q25.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int arr[9] = {1, 1, 2, 2, 6, 5, 8, 8, 5};
+    int i, j;
+    bool check;
+
+    for (i = 0; i < 9; i++)
+    {
+        check = false;
+        for (j = 0; j < 9; j++)
+        {
+            if (i != j && arr[i] == arr[j])
+            {
+                check = true;
+                break;
+            }
+        }
+        if (!check)
+        {
+            printf("%d ", arr[i]);
+            break;
+        }
+    }
+
+    if (i == 9)
+    {
+        printf("No non-duplicate element found.");
+    }
+
+    return 0;
+}
